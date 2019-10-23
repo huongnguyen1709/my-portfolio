9f7e5f7ba81578fd7b76c5d58d8aaf966ead6975
huongnguyen1709 <38068008+huongnguyen1709@users.noreply.github.com>
2019-10-23 15:16:31 +0300

    navigation bar changes, clean code.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-/* JQUERY SMOOTH SCROLL NAV BAR */
-$('.nav-list a, .btn-click').on('click', function(e) {
-    if (this.hash !== '') {
-        e.preventDefault();
-        const hash = this.hash;
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top // scroll from top
-            },
-            800 // speed scroll
-        );
-    }
-});
 
-$('.up-arrow').click(function() {
-    $('html, body').animate({scrollTop: 0}, 800);
-});
+$(document).ready(function() {
+    /* JQUERY SMOOTH SCROLL NAV BAR */
+    $('.btn-click, .nav-link').on('click', function(e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top // scroll from top
+                },
+                800 // speed scroll
+            );
+        }
+    });
+
+    /* UP ARROW BUTTON */ 
+    $('.up-arrow').click(function() {
+        $('html, body').animate({scrollTop: 0}, 800);
+    });
+    
+})
+
+
+
+
+
+
+
+
+
+
